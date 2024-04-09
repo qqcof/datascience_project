73d39370483210c03cfdba676f50baa266aa08f6
Alexey <c-o-f@inbox.ru>
2024-04-09 21:50:59 +0300

    Изменил принцип работы loader.py, теперь данные записываются в БД не из файла, а используются напрямую из get_data_json.py

--- get_data_json.py
+++ get_data_json.py
@@ -1,4 +1,3 @@
-import config
 import json
 import requests
 
@@ -34,16 +33,8 @@ def get_and_save_data(symbol, api_key):
     if 'Monthly Time Series' in data:
         monthly_time_series = data['Monthly Time Series']
         formatted_data = format_data(monthly_time_series)
-
-        with open('nvda_data_formatted.json', 'w') as formatted_file:
-            json.dump(formatted_data, formatted_file, indent=4)
-
-        print('Данные успешно записаны в json файл!')
+        print('Данные успешно получены!')
+        return formatted_data
     else:
         print('Ошибка получения данных!')
-
-
-if __name__ == '__main__':
-    symbol = config.symboltiker
-    api_key = config.apikey
-    get_and_save_data(symbol, api_key)
+        return None
